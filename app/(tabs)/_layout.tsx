9afd5b061d8225af39baf97ff411f6acb4b2e261
nethmidinanjana <nethmidinanjana02@gmail.com>
2024-10-31 23:53:15 +0530

    updated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,9 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import React from "react";
 
-import { TabBarIcon } from '@/components/navigation/TabBarIcon';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { TabBarIcon } from "@/components/navigation/TabBarIcon";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -11,24 +11,47 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-      }}>
+        tabBarStyle: { height: 70, backgroundColor: "#D7E5F0" }, // Sets custom height for the Tab bar
+        tabBarLabelStyle: { paddingTop: 0, marginTop: -10, paddingBottom: 13 }, // Adjust label spacing
+        tabBarItemStyle: { alignItems: 'center' }, // Centers items in each tab
+      }}
+      
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Home',
+          title: "Home",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'home' : 'home-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "home" : "home-outline"}
+              color={color}
+            />
           ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="timer"
         options={{
-          title: 'Explore',
+          title: "Timer",
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon
+              name={focused ? "timer" : "timer-outline"}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: "Account", // Set this to 'Account' to distinguish it from 'Explore'
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon
+              name={focused ? "person" : "person-outline"}
+              color={color}
+            />
           ),
         }}
       />
---
+++ app/(tabs)/timer.tsx
@@ -0,0 +1,14 @@
+
+import { StyleSheet, Image, Platform, View, Text } from 'react-native';
+
+export default function Timer() {
+  return (
+    <View>
+      <Text>Ok</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
